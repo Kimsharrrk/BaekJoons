4a34ef0a68253c55a1a0eceba49b2f567b09c477
Kimsharrrk <108858402+Kimsharrrk@users.noreply.github.com>
2025-08-12 19:29:14 +0900

    baek

--- src/BaekJoon_34071_첫번째문제정말쉬문제일까.java
+++ src/BaekJoon_34071_첫번째문제정말쉬문제일까.java
@@ -7,25 +7,36 @@ public class BaekJoon_34071_첫번째문제정말쉬문제일까 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
 
         int N = Integer.parseInt(bf.readLine());
 
-        int []levelarr = new int[N];
+        int[] levelarr = new int[N];
 
-        for (int i = 0 ; N > i ; i++){
-            int level = Integer.parseInt(bf.readLine());
-            levelarr[i] = level;
-
+        for (int i = 0; N > i; i++) {
+            levelarr[i] = Integer.parseInt(bf.readLine());
         }
 
-        for (int i = 1 ; N >= i ; i ++){
-            if (levelarr[i-1] > levelarr[i]){
+        boolean Bigger = true;
+        boolean Smaller = true;
 
-                System.out.println("ez");
+        for (int i = 0; N > i; i++) {
+
+            if (levelarr[i] >= levelarr[0]) {
+                Smaller = false;
+            }else if (levelarr[i] <= levelarr[0]){
+                Bigger = false;
             }
-
         }
 
+
+        if (Bigger) {
+            System.out.println("ez");
+        } else if (Smaller) {
+            System.out.println("hard");
+        } else {
+            System.out.println("?");
+        }
     }
+
+
 }
